67188ac6dd85c68819c1bfc0b08ff8cc23d96870
Charles <crossan007@gmail.com>
2017-09-03 17:55:14 -0400

    add simlple staff member plugin template

---
+++ src/Halcyonic/single-staff-member.php
@@ -0,0 +1,66 @@
+<?php
+/*
+ Single Staff Member
+ This enables display of staff-member pages from the plugin: https://wordpress.org/plugins/simple-staff-list/
+*/
+get_header(); 
+
+global $post;
+$custom     = get_post_custom();
+$t_name         = get_the_title();
+$name_slug  = basename(get_permalink());
+$title      = $custom["_staff_member_title"][0];
+$email      = $custom["_staff_member_email"][0];
+$phone      = $custom["_staff_member_phone"][0];
+$fax        = $custom["_staff_member_fax"][0];
+$bio        = $custom["_staff_member_bio"][0];
+$prof       = $custom["_staff_member_prof"][0];
+$fb_url     = $custom["_staff_member_fb"][0];
+$tw_handle  =  $custom["_staff_member_tw"][0];
+$tw_url     = 'http://www.twitter.com/' . $tw_handle;
+$li_url     = $custom["_staff_member_li"][0];
+
+if(has_post_thumbnail()){
+    
+   $t_photo_url = wp_get_attachment_url( get_post_thumbnail_id() );
+   $t_photo = '<img class="staff-member-page-photo" style="width:100px;height:100px;" src="'.$t_photo_url.'" alt = "'.$title.'">';
+   }else{
+   $t_photo_url = '';
+   $t_photo = '';
+   }
+  
+$email_mailto = '<a class="staff-member-email" href="mailto:'.antispambot( $email ).'" title="Email '.$name.'">'.antispambot( $email ).'</a>';
+
+?>
+
+<!-- Content -->
+<div id="content-wrapper">
+					<div id="content">
+						<div class="container">
+							<div class="row">
+								<div class="3u 12u(mobile)">
+									<section>
+										<?=$t_photo?>
+										<header><h2><?=$t_name?></h2><h3><?=$title?></h3></header>
+										<ul>
+											<?php if($fb_url) { ?><li><a href="<?=$fb_url?>">Facebook</a></li><?php } ?>
+											<?php if($tw_handle) { ?><li><a href="<?=$tw_url?>">Twitter: @<?= $tw_handle?></a></li><?php } ?>
+											<?php if($email) { ?><li><a href="mailto:<?=$email?>">Email: <?= $email ?></a></li><?php } ?>
+										</ul>
+									</section>
+								</div>
+								<div class="9u 12u(mobile)" id="main-content">
+									<?php if (have_posts() ) : while (have_posts()) : the_post(); ?>
+									<!-- Main Content -->
+										<section>
+											<p class="staff-member-bio"><?=$bio?></p>  
+										</section>
+										
+									<?php endwhile; endif; ?>
+								</div>
+							</div>
+						</div>
+					</div>
+                </div>
+                
+<?= get_footer(); ?>
