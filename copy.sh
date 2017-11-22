13fcbe16b0b79a9c93048a6acf735f626cbcb911
Charles <crossan007@gmail.com>
2017-11-22 13:37:42 -0500

    1.3 tweaks

--- copy.sh
+++ copy.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
 rm -rf public/wp-content/themes/Halcyonic
 cp -R src/Halcyonic public/wp-content/themes
+
+rm -rf public/wp-content/plugins/churchcrm-calendar
+cp -R WordPress-ChurchCRM-Calendar/churchcrm-calendar public/wp-content/plugins/
